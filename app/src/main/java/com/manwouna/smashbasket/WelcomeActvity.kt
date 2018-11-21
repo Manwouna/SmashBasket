67b7fe059b7dbfa988534fc421353dbeb231f762
Manwouna <marioyaou@gmail.com>
2018-11-21 17:33:40 +0300

    LifeCycle

--- app/src/main/java/com/manwouna/smashbasket/WelcomeActvity.kt
+++ app/src/main/java/com/manwouna/smashbasket/WelcomeActvity.kt
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_welcome_actvity.*
 
-class WelcomeActvity : AppCompatActivity() {
+class WelcomeActvity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
