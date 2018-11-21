531d1503ac13cd27215f012db68ca76416a10d16
Manwouna <marioyaou@gmail.com>
2018-11-21 11:23:09 +0300

    initial commit

---
+++ app/src/main/java/com/manwouna/smashbasket/WelcomeActvity.kt
@@ -0,0 +1,12 @@
+package com.manwouna.smashbasket
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeActvity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_actvity)
+    }
+}
