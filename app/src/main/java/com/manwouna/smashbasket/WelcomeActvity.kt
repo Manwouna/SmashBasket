9a1aa5ab31080ef6782090aa977ebd9de2891fe8
Manwouna <marioyaou@gmail.com>
2018-11-21 16:23:47 +0300

    Intent layouts 1.0

--- app/src/main/java/com/manwouna/smashbasket/WelcomeActvity.kt
+++ app/src/main/java/com/manwouna/smashbasket/WelcomeActvity.kt
@@ -1,12 +1,19 @@
 package com.manwouna.smashbasket
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome_actvity.*
 
 class WelcomeActvity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome_actvity)
+
+        getStartedBtn.setOnClickListener {
+            val leagueIntent= Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
